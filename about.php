6d7e03c3b81a16eb66571343a7e8111c1303ea83
Jean-Simon Robert Ouimet <jr@smbsol.com>
2017-03-08 11:12:50 -0500

    + about

--- about.php
+++ about.php
@@ -31,9 +31,8 @@
       <section>
 
         <div class="bit-1">
-          <h2 class="title-small">Summary</h2>
-          <p>Dans une autre vie, il a été organiste titulaire pour la congrégation des Sœurs de Ste-Croix et cofondateur de l&#8217;Ensemble Canto Felice, avec lequel il a donné plusieurs concerts et enregistré un disque. Parallèlement à ses activités d&#8217;interprète, il s&#8217;est attiré l&#8217;attention des médias par ses conférences grand public et ses notes de programme pour des organismes musicaux montréalais tels qu&#8217;Arion orchestre baroque, Clavecin en concert, l&#8217;Orchestre symphonique de Montréal et la Société de musique contemporaine du Québec. Appuyé par le <a href="http://www.frqsc.gouv.qc.ca/" target="_blank" class="link">Fonds de recherche &ndash; Socité et culture&nbsp;&nearr;</a> et le <a href="http://www.sshrc-crsh.gc.ca/home-accueil-fra.aspx" target="_blank" class="link">Conseil de recherches en sciences humaines&nbsp;&nearr;</a>, il a rédigé un mémoire de maîtrise sur la médiation culturelle et son impact sur l&#8217;appréciation de la musique <a href="/assets/pdf/jean-simon-robert-ouimet-la-mediation-musicale.pdf" target="_blank" class="link">read&nbsp;&nearr;</a>.</p>
-          <p>Au terme de ce parcours, il a opéré un virage à 180 degrés et suivi une formation en design graphique au Collège Salette. Après avoir habité à Ottawa pendant un peu plus d&#8217;un an, il est de retour à Montréal où il travaille tant dans l&#8217;imprimé et le web. Sa clientèle est issue de plusieurs milieux tels le droit économique, la mécanique industrielle, la mode, la musique classique, la recherche universitaire et la santé.</p>
+          <p>Welcome to my website. My name is Jean-Simon Robert Ouimet. I am a freelance graphic designer, former musician, and aspiring web developer living in Ottawa, Canada. I had the pleasure to work with clients in various industries: classical music, natural health products, environmental compliance, fashion, and engineering, among others.</p>
+          <p>Whether you are recruiting, looking for a contractor, or would like to collaborate on a new project, feel free to <a href="/contact.php" class="link">contact me&nbsp;&nearr;</a>.</p>
         </div>
 
         <div class="bit-2">
@@ -45,40 +44,30 @@
             <a href="http://smbsol.com/" target="_blank" class="link">Symbiosys&nbsp;&nearr;</a>
           <p>
             <span class="bold">Graphic Designer &amp; Front-end Developer</span><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Concerts by the Canal&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Doors Open for Music at Southminster&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Expert Piano Plateau&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">La Parapharmacie&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Master Piano Recital Series&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Rideau Chorale&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Assent Compliance&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Agence Nancy Paquette&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Foire Écosphère&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Lina Côté&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">KEM Solutions&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">Quadrant Engineering Ltd.&nbsp;&nearr;</a><br>
-            <a href="http://smbsol.com/" target="_blank" class="link">OICRM / Université de Montréal&nbsp;&nearr;</a>
+            <a href="https://www.facebook.com/CBTCOTT/" target="_blank" class="link">Concerts by the Canal&nbsp;&nearr;</a><br>
+            <a href="https://www.facebook.com/MusicatSouthminster/" target="_blank" class="link">Doors Open for Music at Southminster&nbsp;&nearr;</a><br>
+            <a href="https://www.facebook.com/Expert-Piano-Plateau-1072110896143589/" target="_blank" class="link">Expert Piano Plateau&nbsp;&nearr;</a><br>
+            <a href="https://lapara.ca/" target="_blank" class="link">La Parapharmacie&nbsp;&nearr;</a><br>
+            <a href="https://masterpianorecitalseries.com/" target="_blank" class="link">Master Piano Recital Series&nbsp;&nearr;</a><br>
+            <a href="https://www.facebook.com/rideauchorale/" target="_blank" class="link">Rideau Chorale&nbsp;&nearr;</a><br>
+            <a href="http://www.assentcompliance.com/" target="_blank" class="link">Assent Compliance&nbsp;&nearr;</a><br>
+            <a href="https://www.facebook.com/agencenancypaquette/" target="_blank" class="link">Agence Nancy Paquette&nbsp;&nearr;</a><br>
+            <a href="http://foireecosphere.org/" target="_blank" class="link">Foire Écosphère&nbsp;&nearr;</a><br>
+            <a href="http://www.coachdexception.com/" target="_blank" class="link">Lina Côté&nbsp;&nearr;</a><br>
+            <a href="http://kemsolutions.com/" target="_blank" class="link">KEM Solutions&nbsp;&nearr;</a><br>
+            <a href="https://www.quadrantengineering.ca/" target="_blank" class="link">Quadrant Engineering Ltd.&nbsp;&nearr;</a><br>
+            <a href="http://oicrm.org/" target="_blank" class="link">OICRM / Université de Montréal&nbsp;&nearr;</a>
           <p>
 
         </div>
 
         <div class="bit-2">
 
-          <h2 class="title-small">Education</h2>
+          <h2 class="title-small">Languages</h2>
 
           <p>
-            <span class="bold">Attestation of College Studies (ACS) &ndash; Graphic Design</span><br>
-            Collège Salette
-          </p>
-
-          <p>
-            <span class="bold">Master of Arts (MA) &ndash; Music (Musicology)</span><br>
-            Université de Montréal
-          </p>
-
-          <p>
-            <span class="bold">Bachelor of Music (BMus) &ndash; Musicology</span><br>
-            Université de Montréal
+            - HTML / CSS / JavaScript / PHP<br>
+            - Pug (Jade), Sass, Typescript
           </p>
 
           <h2 class="title-small">Certificates</h2>
@@ -96,6 +85,24 @@
           <p>
             <span class="bold">JavaScript Essential Training</span><br>
             <a href="https://www.lynda.com/JavaScript-tutorials/JavaScript-Essential-Training/81266-2.html?certificate=60627FAEEAE3481C8F0B82A1E94CC886" target="_blank" class="link">Lynda.com (60627F)&nbsp;&nearr;</a>
+          </p>
+
+          <h2 class="title-small">Education</h2>
+
+          <p>
+            <span class="bold">Attestation of College Studies (ACS) &ndash; Graphic Design</span><br>
+            Collège Salette
+          </p>
+
+          <p>
+            <span class="bold">Master of Arts (MA) &ndash; Music (Musicology)</span><br>
+            Université de Montréal<br>
+            <a href="/assets/pdf/jean-simon-robert-ouimet-la-mediation-musicale.pdf" target="_blank" class="link">read thesis&nbsp;&nearr;</a>
+          </p>
+
+          <p>
+            <span class="bold">Bachelor of Music (BMus) &ndash; Musicology</span><br>
+            Université de Montréal
           </p>
 
         </div>

aa3c6ee6b29359d95a8807edca696001edccda3c
Jean-Simon Robert Ouimet <jr@smbsol.com>
2017-03-08 23:00:25 -0500

    + google analytics

--- includes/scripts.php
+++ includes/scripts.php
@@ -2,6 +2,7 @@
 <script src="/js/jquery-3.1.1.min.js"></script>
 <script src="/js/jquery.lazyload.js"></script>
 
+<!--Lazyload-->
 <script>
 $(function() {
     $("div.lazy").lazyload({
@@ -10,6 +11,19 @@ $(function() {
 });
 </script>
 
+<!--Clicky-->
 <script src="//static.getclicky.com/js" type="text/javascript"></script>
 <script type="text/javascript">try{ clicky.init(101028743); }catch(e){}</script>
 <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101028743ns.gif" /></p></noscript>
+
+<!--Google Analytics-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-93341008-1', 'auto');
+  ga('send', 'pageview');
+
+</script>

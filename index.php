419eeeb22a856b10fdef8baccfb17a8161fbd549
Jean-Simon Robert Ouimet <jr@smbsol.com>
2017-04-04 16:26:04 -0400

    ~ explore link

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
 
           <a href="/projects/doors-open-for-music-at-southminster.php">
             <div class="bit-3 img-1-1 lazy" data-original="/assets/img/doors-open-for-music-at-southminster/doms-logo-on-image-thumbnail.jpg">
-              <h3 class="grey">explore&nbsp;&nbsp;<span class="icon">&rarr;</span></h3>
+              <h3 style="color: #4C4D4F;">explore&nbsp;&nbsp;<span class="icon">&rarr;</span></h3>
             </div>
           </a>
 

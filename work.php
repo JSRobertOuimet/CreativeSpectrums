a1acb3a752a9ccf50173f1752c3eeb18466bbe24
Jean-Simon Robert Ouimet <jr@smbsol.com>
2017-04-07 17:47:50 -0400

    + misc.

--- work.php
+++ work.php
@@ -32,37 +32,37 @@
           </header>
 
           <a href="/projects/master-piano-recital-series.php">
-            <div class="bit-3 img-1-1 lazy" data-original="/assets/img/master-piano-recital-series/master-piano-recital-series-logo-on-image.jpg">
+            <div class="bit-2 img-1-1 lazy" data-original="/assets/img/master-piano-recital-series/master-piano-recital-series-logo-on-image-thumbnail.jpg">
               <h3>explore&nbsp;&nbsp;<span class="icon">&rarr;</span></h3>
             </div>
           </a>
 
           <a href="/projects/la-parapharmacie.php">
-            <div class="bit-3 img-1-1 lazy" data-original="/assets/img/la-parapharmacie/la-parapharmacie-logo-on-image-thumbnail.jpg">
+            <div class="bit-2 img-1-1 lazy" data-original="/assets/img/la-parapharmacie/la-parapharmacie-logo-on-image-thumbnail.jpg">
               <h3>explore&nbsp;&nbsp;<span class="icon">&rarr;</span></h3>
             </div>
           </a>
 
           <a href="/projects/assent-compliance.php">
-            <div class="bit-3 img-1-1 lazy" data-original="/assets/img/assent-compliance/assent-compliance-logo-on-image.jpg">
+            <div class="bit-2 img-1-1 lazy" data-original="/assets/img/assent-compliance/assent-compliance-logo-on-image.jpg">
               <h3>explore&nbsp;&nbsp;<span class="icon">&rarr;</span></h3>
             </div>
           </a>
 
           <a href="/projects/doors-open-for-music-at-southminster.php">
-            <div class="bit-3 img-1-1 lazy" data-original="/assets/img/doors-open-for-music-at-southminster/doms-logo-on-image-thumbnail.jpg">
+            <div class="bit-2 img-1-1 lazy" data-original="/assets/img/doors-open-for-music-at-southminster/doms-logo-on-image-thumbnail.jpg">
               <h3 style="color: #4C4D4F;">explore&nbsp;&nbsp;<span class="icon">&rarr;</span></h3>
             </div>
           </a>
 
           <a href="/projects/mimib.php">
-            <div class="bit-3 img-1-1 lazy" data-original="/assets/img/mimib/mimib-logo-on-image-thumbnail.jpg">
+            <div class="bit-2 img-1-1 lazy" data-original="/assets/img/mimib/mimib-logo-on-image-thumbnail.jpg">
               <h3>explore&nbsp;&nbsp;<span class="icon">&rarr;</span></h3>
             </div>
           </a>
 
           <a href="/contact.php">
-            <div class="bit-3 img-1-1 your-branding lazy">
+            <div class="bit-2 img-1-1 your-branding lazy">
               <h3>your brand&nbsp;&nbsp;<span class="icon">&rarr;</span></h3>
             </div>
           </a>
@@ -76,66 +76,204 @@
             <h2 class="bit-1 title-small">Logos</h2>
           </header>
 
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/quadrant-engineering-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/la-naturopathe-en-ville-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/echo-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/regions-en-sante-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/quadrant-engineering-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/la-naturopathe-en-ville-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/echo-logo.svg" type="image/svg+xml"></div>
+          
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/regions-en-sante-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/retrofico-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/cote-logo.svg" type="image/svg+xml"></div>
+          
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/officine-des-supplements-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/task-angels-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/libido-guru-logo.svg" type="image/svg+xml"></div>
+          
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/rideau-canal-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/black-swan-nutrition-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/expert-piano-plateau-logo.svg" type="image/svg+xml"></div>
 
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/retrofico-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/cote-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/officine-des-supplements-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/task-angels-logo.svg" type="image/svg+xml"></div>
-
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/libido-guru-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/rideau-canal-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/black-swan-nutrition-logo.svg" type="image/svg+xml"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/expert-piano-plateau-logo.svg" type="image/svg+xml"></div>
-
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/concerts-on-the-canal-logo.svg" type="image/svg+xml"></div>
+          <div class="bit-3 img-1-1 lazy" data-original="/assets/svg/concerts-on-the-canal-logo.svg" type="image/svg+xml"></div>
 
         </section>
 
-        <!--Icons-->
-        <!--<section>
-
-          <header>
-            <h2 class="bit-1 title-small">Icons</h3>
-          </header>
-
-          <a href="#">
-            <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/la-naturopathe-en-ville-icon.svg" type="image/svg+xml"></div>
-          </a>
-
-          <a href="#">
-            <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/officine-des-supplements-icon.svg" type="image/svg+xml"></div>
-          </a>
-
-          <a href="#">
-            <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/task-angels-icon.svg" type="image/svg+xml"></div>
-          </a>
-
-          <a href="#">
-            <div class="bit-4 img-1-1 lazy" data-original="/assets/svg/libido-guru-icon.svg" type="image/svg+xml"></div>
-          </a>
-
-        </section>-->
-
-        <!--Print-->
         <section>
-
+        
           <header>
-            <h2 class="bit-1 title-small">Print</h3>
+            <h2 class="bit-1 title-small">Miscellaneous</h2>
           </header>
 
-          <!--<div class="bit-4 img-1-1 lazy" data-original="/assets/img/other-work/anaq-publicity.jpg"></div>-->
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/img/other-work/angelard-business-card.jpg"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/img/other-work/oicrm-programme.jpg"></div>
-          <!--<div class="bit-4 img-1-1 lazy" data-original="/assets/img/other-work/denis-gougeon-poster.jpg"></div>-->
+          <!--Lucie Charbonneau-->
+          <div class="bit-1 mg-b">
+          
+            <div class="bit-2 img-1-1 lazy" data-original="/assets/img/other-work/all-the-secrets-you-ve-kept-till-now-murale.jpg"></div>
 
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/img/other-work/all-the-secrets-you-ve-kept-till-now-murale.jpg"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/img/other-work/regions-en-sante-business-card.jpg"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="/assets/img/other-work/task-angels-business-card.jpg"></div>
-          <div class="bit-4 img-1-1 lazy" data-original="assets/img/other-work/officine-des-supplements-business-card.jpg"></div>
+            <div class="bit-2 pd-l-d">
+
+              <p>
+                <span class="subtitle">Client</span><br>
+                Private individual
+              </p>
+
+              <p>
+                <span class="subtitle">Industry</span><br>
+                Music
+              </p>
+
+              <p>
+                <span class="subtitle">Deliverable</span><br>
+                - Murale (mixed media), 72 x 72 in
+              </p>
+              
+            </div>
+
+          </div>
+
+          <!--OICRM (Programme)-->
+          <div class="bit-1 mg-b">
+          
+            <div class="bit-2 img-1-1 lazy" data-original="/assets/img/other-work/oicrm-programme.jpg"></div>
+
+            <div class="bit-2">
+
+              <div class="pd-l-d">
+              
+                <p class="mg-b">
+                  <span class="subtitle">Client</span><br>
+                  <a href="http://oicrm.org/" target="_blank" class="link">OICRM&nbsp;&rarr;</a>
+                </p>
+
+                <p class="mg-b">
+                  <span class="subtitle">Industry</span><br>
+                  Music
+                </p>
+
+                <p>
+                  <span class="subtitle">Deliverable</span><br>
+                  - Bilingual programme (60 pages)
+                </p>
+              
+              </div>
+
+              <p class="bit-1 pd-l-d">
+                <a href="/assets/pdf/oicrm-programme.pdf" target="_blank" class="btn left">download programme&nbsp;&nbsp;<span class="icon download">&rarr;</span></a>
+              </p>
+              
+            </div>
+
+          </div>
+
+          <!--Task Angels-->
+          <div class="bit-1 mg-b">
+          
+            <div class="bit-2 img-1-1 lazy" data-original="/assets/img/other-work/task-angels-business-card.jpg"></div>
+
+            <div class="bit-2 pd-l-d">
+
+              <p class="mg-b">
+                <span class="subtitle">Client</span><br>
+                <a href="http://www.taskangels.org/index.html" target="_blank" class="link">Task Angels&nbsp;&rarr;</a>
+              </p>
+
+              <p class="mg-b">
+                <span class="subtitle">Industry</span><br>
+                Renovation, maintenance and moving
+              </p>
+
+              <p>
+                <span class="subtitle">Deliverable</span><br>
+                - New visual identity (logo)<br>
+                - Business cards<br>
+              </p>
+              
+            </div>
+
+          </div>
+
+          <!--OICRM (Poster TRM)-->
+          <div class="bit-1 mg-b">
+          
+            <img class="bit-2 img mg-b border lazy" src="/assets/img/other-work/oicrm-poster-trm.jpg">
+
+            <div class="bit-2 pd-l-d">
+
+              <p class="mg-b">
+                <span class="subtitle">Client</span><br>
+                <a href="http://oicrm.org/" target="_blank" class="link">OICRM&nbsp;&rarr;</a>
+              </p>
+
+              <p class="mg-b">
+                <span class="subtitle">Industry</span><br>
+                Music
+              </p>
+
+              <p>
+                <span class="subtitle">Deliverable</span><br>
+                - Poster<br>
+              </p>
+              
+            </div>
+
+          </div>
+
+          <!--OICRM (Poster Denis Gougeon)-->
+          <div class="bit-1 mg-b">
+          
+            <img class="bit-2 img mg-b border lazy" src="/assets/img/other-work/oicrm-poster-denis-gougeon.jpg">
+
+            <div class="bit-2 pd-l-d">
+
+              <p class="mg-b">
+                <span class="subtitle">Client</span><br>
+                <a href="http://oicrm.org/" target="_blank" class="link">OICRM&nbsp;&rarr;</a>
+              </p>
+
+              <p class="mg-b">
+                <span class="subtitle">Industry</span><br>
+                Music
+              </p>
+
+              <p>
+                <span class="subtitle">Deliverable</span><br>
+                - Poster<br>
+              </p>
+              
+            </div>
+
+          </div>
+
+          <!--KEM (illustration)-->
+          <div class="bit-1 mg-b">
+          
+            <img class="bit-2 img lazy" src="/assets/img/other-work/kem-illustration.jpg">
+
+            <div class="bit-2">
+
+              <div class="pd-l-d">
+              
+                <p class="mg-b">
+                  <span class="subtitle">Client</span><br>
+                  KEM Solutions
+                </p>
+
+                <p class="mg-b">
+                  <span class="subtitle">Industry</span><br>
+                  Health
+                </p>
+
+                <p>
+                  <span class="subtitle">Deliverable</span><br>
+                  - Illustrated advertisement (print)
+                </p>
+              
+              </div>
+
+              <p class="bit-1 pd-l-d">
+                <a href="/assets/pdf/kem-illustration.pdf" target="_blank" class="btn left">download ad&nbsp;&nbsp;<span class="icon download">&rarr;</span></a>
+              </p>
+              
+            </div>
+
+          </div>
 
         </section>
 

57d030200548dd5bd6f5c7cbc406f68dacb20779
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2019-06-12 17:34:48 -0400

    Udpated footer social links

--- includes/footer.php
+++ includes/footer.php
@@ -5,23 +5,17 @@
       <p class="copy">&copy; Creative Spectrums <?php echo date("Y"); ?></p>
     </div>
     <div class="right">
-      <a href="https://www.facebook.com/CreativeSpectrums/" target="_blank">Facebook</a>
-      <!--<a href="https://twitter.com/CrtvSpctrms" target="_blank">Twitter</a>-->
-      <a href="https://www.linkedin.com/company-beta/10334195" target="_blank">LinkedIn</a>
-      <!--<a href="https://plus.google.com/u/1/114899720654980054505" target="_blank">Google+</a>-->
-      <!--<a href="https://www.instagram.com/creativespectrums/" target="_blank">Instagram</a>-->
-      <!--<a href="https://www.pinterest.com/CrtvSpctrms/" target="_blank">Pinterest</a>-->
+      <a href="https://github.com/JSRobertOuimet" target="_blank">GitHub</a>
+      <a href="https://codepen.io/JSRobertOuimet/" target="_blank">CodePen</a>
+      <a href="https://www.linkedin.com/in/jsrobertouimet/" target="_blank">LinkedIn</a>
     </div>
   </nav>
 </footer>
 
 <!--Mobile Footer-->
 <footer id="main-footer" class="bit-1 mobile-only">
-  <a href="https://www.facebook.com/CreativeSpectrums/" target="_blank" class="bit-1 center no-mg">Facebook</a>
-  <!--<a href="https://twitter.com/CrtvSpctrms" target="_blank" class="bit-1 center no-mg">Twitter</a>-->
-  <a href="https://www.linkedin.com/company-beta/10334195" target="_blank" class="bit-1 center no-mg">LinkedIn</a>
-  <!--<a href="https://plus.google.com/u/1/114899720654980054505" target="_blank" class="bit-1 center no-mg">Google+</a>-->
-  <!--<a href="https://www.instagram.com/creativespectrums/" target="_blank" class="bit-1 center no-mg">Instagram</a>-->
-  <!--<a href="https://www.pinterest.com/CrtvSpctrms/" target="_blank" class="bit-1 center no-mg">Pinterest</a>-->
+  <a href="https://github.com/JSRobertOuimet" target="_blank" class="bit-1 center no-mg">GitHub</a>
+  <a href="https://codepen.io/JSRobertOuimet/" target="_blank" class="bit-1 center no-mg">CodePen</a>
+  <a href="https://www.linkedin.com/in/jsrobertouimet/" target="_blank" class="bit-1 center no-mg">LinkedIn</a>
   <p class="bit-1 copy center">&copy; Creative Spectrums <?php echo date("Y"); ?></p>
 </footer>

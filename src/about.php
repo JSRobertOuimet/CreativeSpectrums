a2ed6440ff873b0db1f171d9e059fed44399dc28
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2019-06-29 10:40:59 -0400

    Updated the About section

--- src/about.php
+++ src/about.php
@@ -22,21 +22,21 @@
             <h2 class="title-small">Experience</h2>
             <p>
               <a href="http://creativespectrums.com/" class="link">Creative Spectrums</a><br>
-              <span class="bold">Graphic Designer, Web Designer & Developer (From 2013)</span><br>
+              <span class="bold">Designer & Developer (Since 2013)</span><br>
               Gather requirements and define project scope within timeline and budget. Build profitable long-term relationships by providing excellent customer service. Create visual identities and brand guidelines for a variety of industries. Trace existing bitmap logos to vector graphics. Design administrative documents and marketing material for print and web. Design, develop and maintain websites.
             </p>
             <p>
               <a href="https://kivuto.com/" target="_blank" class="link">Kivuto Solutions</a><br>
-              <span class="bold">UI/UX Designer (September 2018 &ndash; June 2019)</span><br>
+              <span class="bold">UI/UX Designer (Sept. 2018 &ndash; June 2019)</span><br>
               Worked on ELMS, a multilingual enterprise-level application (50 million users). Ran user interviews to understand usage, pain points, and define personas. Created interactive prototypes to illustrate simplified user flows. Presented findings of user research and recommendations to senior management and project stakeholders. Designed functionality to collect, display and manage payment methods and addresses to support a new subscription-based billing model. Created and maintained a pattern library to accelerate design and development process while facilitating communication with stakeholders and QA testers.
             </p>
             <p>
-              <span class="bold">Front-end Web Developer (June 2017 &ndash; August 2018)</span><br>
+              <span class="bold">Front-end Web Developer (June 2017 &ndash; Aug. 2018)</span><br>
               Worked on Texidium, a multilingual e-reader consumer application. Conducted a UI inventory across five clients using atomic design principles. Worked in tandem with developers to modernized the UI of the application. Designed and implemented new features. Enhanced existing features based on user feedback. Defined requirements for a read aloud feature using user scenarios.
             </p>
             <p>
               <a href="https://www.smbsol.com/" target="_blank" class="link">Symbiosys</a><br>
-              <span class="bold">Front-end Web Developer (June 2016 &ndash; September 2016)</span><br>
+              <span class="bold">Front-end Web Developer (June 2016 &ndash; Sept. 2016)</span><br>
               Implemented user interfaces from provided mock-ups for three hybrid applications: an investment management app, a video chat app based on mutual interests, and an online classified advertising app.
             </p>
           </div>
@@ -45,15 +45,15 @@
             <h2 class="title-small">Education</h2>
             <p>
               <a href="https://collegesalette.com/" target="_blank" class="link">Collège Salette</a><br>
-              <span class="bold">Attestation of College Studies (ACS) &ndash; Graphic Design</span>
+              <span class="bold">Attestation of College Studies &mdash; Graphic Design (2014)</span>
             </p>
             <p>
               <a href="https://www.umontreal.ca/en/" target="_blank" class="link">Université de Montréal</a><br>
-              <span class="bold">Master of Arts (MA) &ndash; Music (Musicology)</span><br>
+              <span class="bold">Master of Arts &mdash; Music, Musicology (2012)</span><br>
             </p>
             <p>
               <a href="https://www.umontreal.ca/en/" target="_blank" class="link">Université de Montréal</a><br>
-              <span class="bold">Bachelor of Music (BMus) &ndash; Musicology</span></span>
+              <span class="bold">Bachelor of Music &mdash; Musicology (2010)</span></span>
             </p>
           </div>
           <p class="bit-1 mg-t">

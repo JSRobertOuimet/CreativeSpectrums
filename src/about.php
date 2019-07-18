1c86a7750990c933addefea56a414ecd79ba4019
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2019-07-18 12:04:18 -0400

    Updated about section and resume

--- src/about.php
+++ src/about.php
@@ -16,28 +16,32 @@
         </header>
         <section>
           <div class="bit-1">
-            <p>My name is Jean-Simon Robert Ouimet, and I am a passionate visual designer with a strong inclination towards branding and front-end web development. I like to translate business requirements into seamless experiences informed by user research and brand recognition. I love to turn those experiences into elegant interfaces using the latest technologies. I enjoy both working independently or as part of a team where cooperation is the common denominator. My mother tongue is French and I’m proudly bilingual. I’m a former musician who persistently strives to create that pitch-perfect moment that will make a positive change for my audience.</p>
+            <p>Independent thinker seeking innovative solutions to complex problems.</p>
+            <p>Working as a professional musician for more than ten years taught me that the most valuable things in life can only be achieved through dedication, and it is in the same spirit that I approach my work as a designer today. I love to be surrounded by smart, yet humble people, and I will do everything I can to help them leverage their strengths in an effort to materialize a vision.</p>
+            <p>I have a flair for creating memorable visual identities, impactful marketing material, and elegant user interfaces. My work has been praised for both its originality and clarity. I know my way around most wireframing and prototyping tools, but I also like to use good old pen and paper. My strong desire to understand how things work under the hood led me to web development. Over the years, I have worked with AngularJS, Ionic, MongoDB, Node.js, React, and Redux. I enjoy learning new frameworks and workflows, and during my spare time, I am usually reading some documentation.</p>
+            <p>A day is too short to keep up with all my interests. I have played many competitive sports, including badminton, ping-pong, and chess — yes, chess is a sport. I also practiced taekwondo for many years. These days, I nourish a passion for motor racing and try to be on the track whenever possible.</p>
+            <p>My mother tongue is French and I am proudly bilingual.</p>
           </div>
           <div class="bit-1">
             <h2 class="title-small">Experience</h2>
             <p>
               <a href="http://creativespectrums.com/" class="link">Creative Spectrums</a><br>
               <span class="bold">Designer & Developer (Since 2013)</span><br>
-              Gather requirements and define project scope within timeline and budget. Build profitable long-term relationships by providing excellent customer service. Create visual identities and brand guidelines for a variety of industries. Trace existing bitmap logos to vector graphics. Design administrative documents and marketing material for print and web. Design, develop and maintain websites.
+              Gather requirements and define project scope that respects timeline and budget. Build profitable partnerships by providing excellent customer service. Create visual identities and brand guidelines for clients evolving in a variety of industries. Trace existing bitmap logos to vector graphics. Design administrative documents and marketing material for print and web. Design, develop and maintain websites.
             </p>
             <p>
               <a href="https://kivuto.com/" target="_blank" class="link">Kivuto Solutions</a><br>
               <span class="bold">UI/UX Designer (Sept. 2018 &ndash; June 2019)</span><br>
-              Worked on ELMS, a multilingual enterprise-level application (50 million users). Ran user interviews to understand usage, pain points, and define personas. Created interactive prototypes to illustrate simplified user flows. Presented findings of user research and recommendations to senior management and project stakeholders. Designed functionality to collect, display and manage payment methods and addresses to support a new subscription-based billing model. Created and maintained a pattern library to accelerate design and development process while facilitating communication with stakeholders and QA testers.
+              Worked on ELMS, a multilingual enterprise-level application (50M users). Ran user interviews that highlighted usability issues and informed recommendations presented to stakeholders and senior management, orienting future development of the application. Designed multiple features that supported a new subscription-based business model. Created a modern design system that harmonized the user interface of the application, accelerated iteration process, and facilitated communication with QA testers, stakeholders and senior management.
             </p>
             <p>
               <span class="bold">Front-end Web Developer (June 2017 &ndash; Aug. 2018)</span><br>
-              Worked on Texidium, a multilingual e-reader consumer application. Conducted a UI inventory across five clients using atomic design principles. Worked in tandem with developers to modernized the UI of the application. Designed and implemented new features. Enhanced existing features based on user feedback. Defined requirements for a read aloud feature using user scenarios.
+              Worked on Texidium, a multilingual e-reader consumer application. Designed and implemented new features based on user feedback. Resolved bugs and usability issues. Worked in tandem with developers to harmonize the user interface of the application across four operating systems (Android, iOS, Windows and macOS).
             </p>
             <p>
               <a href="https://www.smbsol.com/" target="_blank" class="link">Symbiosys</a><br>
               <span class="bold">Front-end Web Developer (June 2016 &ndash; Sept. 2016)</span><br>
-              Implemented user interfaces from provided mock-ups for three hybrid applications: an investment management app, a video chat app based on mutual interests, and an online classified advertising app.
+              Turned provided mockups into data-driven, scalable, and accessible user interfaces using a variety of frameworks (AngularJS, Angular Material, Bootstrap, and Ionic). Worked on an investment management app, a video chat app based on mutual interests, and an online classified advertising app.
             </p>
           </div>
 
